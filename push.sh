6405082de98928333bf0e4c0d27091a8d8f1e2ea
Evan Sailer <esailer@asu.edu>
2019-03-29 23:40:47 -0500

    Update push.sh

--- push.sh
+++ push.sh
@@ -3,11 +3,4 @@
 set -x
 set -eo pipefail
 
-docker push grahamdumpleton/mod-wsgi-docker:python-2.7
-docker push grahamdumpleton/mod-wsgi-docker:python-2.7-onbuild
-docker push grahamdumpleton/mod-wsgi-docker:python-3.3
-docker push grahamdumpleton/mod-wsgi-docker:python-3.3-onbuild
-docker push grahamdumpleton/mod-wsgi-docker:python-3.4
-docker push grahamdumpleton/mod-wsgi-docker:python-3.4-onbuild
-docker push grahamdumpleton/mod-wsgi-docker:python-3.5
-docker push grahamdumpleton/mod-wsgi-docker:python-3.5-onbuild
+docker push esailer/mod-wsgi-docker:python-3.5
